8f32aa67f075887480f1daceb234a4b41557a403
Sean Steimer <ssteimer@adobe.com>
2025-07-22 10:53:35 -0700

    feat: add resource registration

--- src/operations/resource-templates/index.js
+++ src/operations/resource-templates/index.js
@@ -1,1 +1,16 @@
-export default function defineResourceTemplates(server) {}
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+
+const resourceTemplates = [];
+
+/**
+ * Register resources with the MCP server.
+ * 
+ * @param {McpServer} server 
+ */
+export default function registerResourceTemplates(server) {
+  resourceTemplates.forEach((resourceTemplate) => {
+    const { name, template, config, handler } = resourceTemplate;
+
+    server.registerResource(name, template, config, handler);
+  });
+}
--- src/operations/resources/index.js
+++ src/operations/resources/index.js
@@ -1,1 +1,16 @@
-export default function defineResources(server) {}
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+
+const resources = [];
+
+/**
+ * Register resources with the MCP server.
+ * 
+ * @param {McpServer} server 
+ */
+export default function registerResources(server) {
+  resources.forEach((resource) => {
+    const { name, uri, config, handler } = resource;
+
+    server.registerResource(name, uri, config, handler);
+  });
+}
